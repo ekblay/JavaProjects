ec197ee322acdf7743c4a6464b5ee2a129ce5ab6
ekblay <43041985+ekblay@users.noreply.github.com>
2019-01-17 00:32:09 -0400

    Add files via upload

---
+++ Hotel Webpage/ContactUs.php
@@ -0,0 +1,35 @@
+/**
+* Created by PhpStorm.
+* User: Emmanuel Blay
+* Date: 16/11/2018
+* Time: 23:35
+*/
+<!DOCTYPE html>
+
+<!-- Fig. 2.3: links.html -->
+<!-- Linking to other web pages. -->
+<html>
+<head>
+    <meta charset = "utf-8">
+    <title>Contact Us</title>
+</head>
+<?php
+
+print("   <body>
+      <h1>Thanks for stopping you can reach us if you have any questions or concerns</h1>
+
+       
+                
+<ul>
+                                <li>email: <a href = 'mailto:housestark@winterfell.com'> housestark@winterfell.com</a></li>
+                                <li>Phone: (555)-555-5555</li>
+                        </ul>
+                
+        </ul> 
+        
+   </body>
+
+   <p><a href = 'index.php'> Make a Booking.</a></p>
+     
+</html>");
+
